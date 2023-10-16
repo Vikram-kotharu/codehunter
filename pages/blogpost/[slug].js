bf8259d6f44a5faf4f1e2c417753394dc54fe2a9
vikram <adityavikram797@gmail.com>
2023-10-16 16:16:28 +0530

    added more

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-const Slug = () => {
-  const [fdata, blogging] = useState([])
-  const router = useRouter();
-  const { slug } = router.query;
+const Slug = (props) => {
+  const [fdata, blogging] = useState(props.data)
+  console.log(props)
+  //const router = useRouter();
+  //const { slug } = router.query;
   
-  const finaldata = async()=>{
-    const response = await fetch(`http://localhost:3000/api/getblogs?vikram=${slug}`)
-    const data = await response.json()
-    blogging(data)
-  }
-  useEffect(()=>{
-    finaldata()
-  },[])
+  //const finaldata = async()=>{
+    //const response = await fetch(`http://localhost:3000/api/getblogs?vikram=${slug}`)
+    //const data = await response.json()
+    //blogging(data)
+  //}
+  //useEffect(()=>{
+    //finaldata()
+  //},[])
   console.log(fdata)
   return (
     <>
@@ -31,5 +32,15 @@ const Slug = () => {
     </>
   );
 };
+
+export async function getServerSideProps(context){
+  const {slug} = context.query
+  const response = await fetch(`http://localhost:3000/api/getblogs?vikram=${slug}`)
+  const data = await response.json()
+  return{
+    props:{data}
+
+  }
+}
 
 export default Slug;
